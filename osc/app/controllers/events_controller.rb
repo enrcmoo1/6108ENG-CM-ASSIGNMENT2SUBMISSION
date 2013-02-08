b4a13d6f5095d2b29c0e61ae5de14fa884029bcd
enrcmoo1 <C.Moore1@2010.ljmu.ac.uk>
2013-02-08 14:00:38 +0000

    AS2 Submission Commit

--- osc/app/controllers/events_controller.rb
+++ osc/app/controllers/events_controller.rb
@@ -1,44 +1,25 @@
 class EventsController < ApplicationController
-  # GET /events
-  # GET /events.json
+
   def index
     @events = Event.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @events }
     end
-  end
+  
 
-  # GET /events/1
-  # GET /events/1.json
+
   def show
     @event = Event.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @event }
     end
-  end
+  
 
-  # GET /events/new
-  # GET /events/new.json
   def new
     @event = Event.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @event }
     end
-  end
+  
 
-  # GET /events/1/edit
   def edit
     @event = Event.find(params[:id])
   end
 
-  # POST /events
-  # POST /events.json
   def create
     @event = Event.new(params[:event])
 
@@ -53,10 +34,11 @@ class EventsController < ApplicationController
     end
   end
 
-  # PUT /events/1
-  # PUT /events/1.json
   def update
     @event = Event.find(params[:id])
+    redirect_to event_path(@event)
+
+    flash.notice = "#{@event.title}updated"
 
     respond_to do |format|
       if @event.update_attributes(params[:event])
@@ -69,15 +51,9 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event = Event.find(params[:id])
     @event.destroy
-
-    respond_to do |format|
-      format.html { redirect_to events_url }
-      format.json { head :no_content }
+    redirect_to Events_path(@events)
     end
-  end
 end
--- osc/app/controllers/welcome_controller.rb
+++ osc/app/controllers/welcome_controller.rb
@@ -5,9 +5,6 @@ class WelcomeController < ApplicationController
   def about
   end
 
-  def access
-  end
-
   def holidays
   end
 
